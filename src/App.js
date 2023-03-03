f1737e8bd698b89140cdba011f7ed4513ef97752
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-03-02 23:42:41 -0500

    Add Home, Projects, Experience pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

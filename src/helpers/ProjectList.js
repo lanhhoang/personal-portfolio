c14215632f302d5fd881e3a80a88d99fe0d935fa
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-03-08 10:33:49 -0500

    Create Project Card component

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,18 @@
+import PigGame from "../assets/img/pig-game.jpg";
+import GuessMyNumber from "../assets/img/guess-my-number.jpg";
+import Forkify from "../assets/img/forkify.jpg";
+
+export const ProjectList = [
+  {
+    name: "Pig Game",
+    image: PigGame,
+  },
+  {
+    name: "Guess My Number",
+    image: GuessMyNumber,
+  },
+  {
+    name: "Forkify",
+    image: Forkify,
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
+
+import "../styles/Projects.scss";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;

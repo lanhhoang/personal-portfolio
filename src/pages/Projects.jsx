0876a2dc147dd8db4b253c4364975505a3462183
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-04-26 17:39:00 -0400

    Navigate project item to project display

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,8 +9,10 @@ function Projects() {
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        {ProjectList.map((project) => {
-          return <ProjectItem name={project.name} image={project.image} />;
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
         })}
       </div>
     </div>

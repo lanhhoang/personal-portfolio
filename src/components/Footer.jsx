c2ea14bb455a6794c101b1e0523c8f61f8f7e9e6
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-04-29 16:36:09 -0400

    Change Twitter to LeetCode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import TwitterIcon from "@material-ui/icons/Twitter";
+import CodeIcon from "@material-ui/icons/Code";
 import "../styles/Footer.scss";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <IconButton href="https://github.com/lanhhoang" target="_blank">
-          <GitHubIcon />
-        </IconButton>
         <IconButton href="https://www.linkedin.com/in/lanh" target="_blank">
           <LinkedInIcon />
         </IconButton>
-        <IconButton href="https://twitter.com/luka5h" target="_blank">
-          <TwitterIcon />
+        <IconButton href="https://github.com/lanhhoang" target="_blank">
+          <GitHubIcon />
+        </IconButton>
+        <IconButton href="https://leetcode.com/lanhhoang" target="_blank">
+          <CodeIcon />
         </IconButton>
       </div>
       <p>&copy; 2023 lukash.dev</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import TwitterIcon from "@material-ui/icons/Twitter";
+import CodeIcon from "@material-ui/icons/Code";
 import "../styles/Home.scss";
 
 function Home() {
@@ -12,14 +12,14 @@ function Home() {
         <h2>Hi, My name is Lukas</h2>
         <div className="prompt">
           <p>A software developer with a passion for leaning and creating</p>
-          <IconButton href="https://github.com/lanhhoang" target="_blank">
-            <GitHubIcon />
-          </IconButton>
           <IconButton href="https://www.linkedin.com/in/lanh" target="_blank">
             <LinkedInIcon />
           </IconButton>
-          <IconButton href="https://twitter.com/luka5h" target="_blank">
-            <TwitterIcon />
+          <IconButton href="https://github.com/lanhhoang" target="_blank">
+            <GitHubIcon />
+          </IconButton>
+          <IconButton href="https://leetcode.com/lanhhoang" target="_blank">
+            <CodeIcon />
           </IconButton>
         </div>
       </div>

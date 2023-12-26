aafc2c918a5193618604016d5007a6ddd39f361d
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-26 00:49:14 -0500

    Query & display individual project

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -19,8 +19,30 @@ export async function getProjects(): Promise<Project[]> {
       name,
       "slug": slug.current,
       "image": image.asset->url,
-      alt,
       url
     }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId,
+    dataset,
+    apiVersion: "2023-12-24",
+    useCdn: true,
+  });
+
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      "alt": image.alt,
+      url,
+      description
+    }`,
+    { slug }
   );
 }

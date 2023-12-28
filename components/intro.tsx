723dcb3913fad61b20215ae16d85b4946b67b961
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-28 10:46:44 -0500

    Add intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -39,6 +39,18 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Lucas.</span> I&apos;m a{" "}
+        <span className="font-bold">full stack developer</span> with{" "}
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
     </section>
   );
 }

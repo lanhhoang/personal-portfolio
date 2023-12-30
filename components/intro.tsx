e6e7224a7877fa043f50d45dec0acabb1136dc82
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-29 23:57:25 -0500

    Change p to h1 in Intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,13 +66,13 @@ export default function Intro() {
             </div>
           </div>
 
-          <motion.p
+          <motion.h1
             className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
           >
             <PortableText value={profile.intro} />
-          </motion.p>
+          </motion.h1>
 
           <motion.div
             className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"

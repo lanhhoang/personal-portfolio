a6d1a65d69fb768f88eeab944caadbad67383b43
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-30 12:01:51 -0500

    Add Projects component

---
+++ components/projects.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import Project from "./project";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <Fragment key={index}>
+            <Project {...project} />
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}

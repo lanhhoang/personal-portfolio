54000447a110db2c4cba505ee34300d9da0e97fb
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-30 00:13:40 -0500

    Add SectionHeading component

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import SectionHeading from "./section-heading";
+
 export default function About() {
   return (
     <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
-      <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Accounting</span>, I decided to pursue my

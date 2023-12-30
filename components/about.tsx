90ca7c64c3e4e0ed1cdda9717996c10351de1723
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-30 00:29:05 -0500

    Query & display profile about

--- components/about.tsx
+++ components/about.tsx
@@ -1,42 +1,47 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
+import { Profile } from "@/types/Profile";
+import { getProfile } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
 
 export default function About() {
+  const [profile, setProfile] = useState<Profile | null>(null);
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  const fetchProfile = async () => {
+    try {
+      const data = await getProfile("lucas");
+      setProfile(data);
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+    }
+  };
+
   return (
-    <motion.section
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-    >
-      <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-      <p>
-        <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className="font-medium">learning new things</span>. I am
-        currently learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I&apos;m
-        also learning how to play the guitar.
-      </p>
-    </motion.section>
+    <div>
+      {profile ? (
+        <motion.section
+          className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.175 }}
+        >
+          <SectionHeading>About me</SectionHeading>
+          <p className="mb-3">
+            <PortableText value={profile.about} />
+          </p>
+        </motion.section>
+      ) : (
+        <div className="flex items-center justify-center rounded-full w-14 h-14 bg-gradient-to-tr from-indigo-500 to-pink-500">
+          <div className="h-9 w-9 rounded-full bg-gray-200"></div>
+        </div>
+      )}
+    </div>
   );
 }

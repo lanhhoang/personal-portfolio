2dcb83f09f585556c4ee87bc6730d19f35534cc8
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-01-07 12:10:01 -0500

    Add Experience component

---
+++ components/experience.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Fragment, useState, useEffect } from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+
+export default function Experience() {
+  return (
+    <section id="experience">
+      <SectionHeading>Experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((experience, index) => (
+          <Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0,0,0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+                visibility: "visible",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={experience.date}
+              icon={experience.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+                visibility: "visible",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{experience.title}</h3>
+              <p className="font-normal !mt-0">{experience.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">
+                {experience.description}
+              </p>
+            </VerticalTimelineElement>
+          </Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}

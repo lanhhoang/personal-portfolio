f0f5ae2d0e6cd90ca62f34e2f682efbe0ced4077
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-01-11 23:20:24 -0500

    Use Resend service to send email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,39 @@
 "use server";
 
+import { error } from "console";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const validateString = (value: unknown) => {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+
+  return true;
+};
+
 export const sendEmail = async (formData: FormData) => {
-  console.log("Running on server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  if (validateString(senderEmail)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+
+  if (validateString(message)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "lanhhoang.dev@gmail.com",
+    subject: "Message from contact form",
+    reply_to: senderEmail,
+    text: message,
+  });
 };

774feaeae7c1f9df110a58afd966073e195a6232
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-01-13 23:10:25 -0500

    Add getErrorMessage function to handle error

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,39 +1,40 @@
 "use server";
 
-import { error } from "console";
 import { Resend } from "resend";
+import { getErrorMessage, validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const validateString = (value: unknown) => {
-  if (!value || typeof value !== "string") {
-    return false;
-  }
-
-  return true;
-};
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  if (validateString(senderEmail)) {
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
     return {
       error: "Invalid sender email",
     };
   }
 
-  if (validateString(message)) {
+  if (!validateString(message, 5000)) {
     return {
       error: "Invalid message",
     };
   }
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "lanhhoang.dev@gmail.com",
-    subject: "Message from contact form",
-    reply_to: senderEmail,
-    text: message,
-  });
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "lanhhoang.dev@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    console.error(error);
+
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };

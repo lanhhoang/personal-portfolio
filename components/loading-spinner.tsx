30f6f64ae004b677cd4e2a0a08c6113f53163f0c
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-01-21 20:10:24 -0500

    Add dark styling to LoadingSpinner

--- components/loading-spinner.tsx
+++ components/loading-spinner.tsx
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center rounded-full w-14 h-14 bg-gradient-to-tr from-indigo-500 to-pink-500">
-      <div className="h-9 w-9 rounded-full bg-gray-200"></div>
+    <div className="flex items-center justify-center rounded-full w-14 h-14 bg-gradient-to-tr from-indigo-500 to-pink-500 dark:from-indigo-800 dark:to-pink-600">
+      <div className="h-9 w-9 rounded-full bg-gray-200 dark:bg-gray-600"></div>
     </div>
   );
 }
